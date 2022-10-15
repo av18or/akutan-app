315cd49f876ee7b2462693f47c843cb3540a95a4
Clayton <claytongorham@gmail.com>
2022-10-15 11:10:46 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './Png7.png';
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
+    
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
+        <p>Great things coming soon!</p>
       </header>
     </div>
+
+   
+    
   );
+  
 }
 
-export default App;
+
